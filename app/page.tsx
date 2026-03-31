cb93c35bdf234fa23a9f9163b710be9939941927
Pedro Manoel <ordep.manoel4@gmail.com>
2026-03-31 00:11:54 -0300

    COnfigurando robô do CI

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-scren flex-col items-center p-24">
+      <h1 className="text-4x1 font-bold text-blue-600">
+        Pousada Pedra Furada - Sistema de Gestão
+      </h1>
+      <p className="mt-4 text-x1 text-gray-600">
+        Bem-Vindo ao projeto do semestre!
+      </p>
+    </main>
+
+
+
+
+
+
+  )
+}
